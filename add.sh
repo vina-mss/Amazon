18d008e855c8c7129338d8d4dbbb71375c4a77cb
elangovan0 <elangovan.k@hotmail.com>
2022-09-24 19:30:19 +0000

    commit update on add.sh only

--- add.sh
+++ add.sh
@@ -1,5 +1,6 @@
 echo ""
 echo "shell program to add 2 values"
+echo "this shelll script created by prakash mani"
 echo ""
 read -p "enter the value of a is: " a
 read -p "enter the value of b is: " b
