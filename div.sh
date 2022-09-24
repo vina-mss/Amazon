8b2cd10b5e4c51f3b84feed380bac48d0145acde
elangovan0 <elangovan.k@hotmail.com>
2022-09-24 19:07:12 +0000

    commit div.sh

---
+++ div.sh
@@ -0,0 +1,10 @@
+echo ""
+echo "shell program to div 2 values"
+echo ""
+read -p "enter the value of a is: " a
+read -p "enter the value of b is: " b
+echo ""
+c=`expr $a / $b`
+echo ""
+echo "value of a / b is: $c"
+echo ""
