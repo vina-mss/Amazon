1c22417fde0c5a0a91df8f50d009243bcd467f0b
elangovan0 <112743516+elangovan0@users.noreply.github.com>
2022-09-25 19:03:29 +0530

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -1,6 +1,5 @@
 echo ""
 echo "shell program to sub 2 values"
-echo "this shell script is created by Elangovan"
 echo ""
 read -p "enter the value of a is: " a
 read -p "enter the value of b is: " b
